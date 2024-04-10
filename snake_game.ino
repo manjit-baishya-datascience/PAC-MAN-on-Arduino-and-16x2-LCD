fb312e1ce5dd053880a89a00083cab47d12fb255
Manjit Baishya <manjitbaishya01@gmail.com>
2024-04-10 08:14:13 +0530

    source updated

--- snake_game.ino
+++ snake_game.ino
@@ -1,39 +1,46 @@
+// importing required libraries
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Set the LCD address to 0x27 or whatever your I2C address is
+// setting LCD address to 0x27
 #define I2C_ADDR 0x27
 
-// Set the LCD dimensions (16 columns and 2 rows)
+// setting the LCD dimensions (16 columns and 2 rows)
 #define LCD_COLUMNS 16
 #define LCD_ROWS 2
 
-// Initialize the LCD object
+// initializing the LCD object
 LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_ROWS);
 
+// defining input pins for input
 const int Btn_1 = 2;
 const int Btn_2 = 4;
 const int Btn_3 = 7;
 const int Btn_4 = 8;
 
+// defining variables to store data throughout the game
 int counter = 0;
-int match = -1;
-int score = 0;
+int match = 0;
+int score = -1;
 
+// current position of snake
 int X = 0;
 int Y = 0;
 
+// previous position of snake
 int prev_X = 0;
 int prev_Y = 0;
 
+// current position of monster
 int monster_X = 0;
 int monster_Y = 0;
 
+// previous position of monster
 int monster_prev_X = 0;
 int monster_prev_Y = 0;
 
-// Define custom character bitmap for a single dot
-byte block[8] = {
+// defining custom character bitmap for snake
+byte snake[8] = {
   B11111,
   B11111,
   B11111,
@@ -44,6 +51,7 @@ byte block[8] = {
   B11111
 };
 
+// defining custom character bitmap for monster
 byte monster[8] = {
   B00100,
   B01110,
@@ -55,52 +63,60 @@ byte monster[8] = {
   B11111
 };
 
+// setting up the main program
 void setup() {
 
+  // defining the pinmodes for all input and pins
   pinMode(Btn_1, INPUT);
   pinMode(Btn_2, INPUT);
   pinMode(Btn_3, INPUT);
   pinMode(Btn_4, INPUT);
 
+  // initializing the LCD
   lcd.init();
   lcd.backlight();
 
+  // printing message and score
   lcd.setCursor(12, 0);
   lcd.print("|Tot");
-
   lcd.setCursor(12, 1);
   lcd.write('|');
 
-  lcd.setCursor(13, 1);
-  lcd.print(score);
-
+  // randomizing the value
   randomSeed(analogRead(0));
 }
 
+// defining the loop sequence
 void loop() {
+
+  // reading input pins
   int Btn_1_state = digitalRead(Btn_1);
   int Btn_2_state = digitalRead(Btn_2);
   int Btn_3_state = digitalRead(Btn_3);
   int Btn_4_state = digitalRead(Btn_4);
 
+  // setting conditions for positioning snake
   if(X == monster_X && Y == monster_Y){
     match = 1;
     score++;
   }
 
+  // update score
   lcd.setCursor(13, 1);
   lcd.print(score);
 
-  // counter
+  // randomly placing snake
   if(counter == 0 || match == 1){
     match = 0;
     counter = 20;
 
+    // clearing previous position of snake
     monster_prev_X = monster_X;
     monster_prev_Y = monster_Y;
     lcd.setCursor(monster_prev_X, monster_prev_Y);
     lcd.write(' ');
 
+    // printing new position of snake
     monster_X = random(0, 12);
     monster_Y = random(0, 2);
     lcd.createChar(0, monster);
@@ -108,8 +124,10 @@ void loop() {
     lcd.write(byte(0)); 
   }
 
+  // decrementing counter
   counter--;
 
+  // on pressing button 1 -> go left
   if(Btn_1_state == HIGH) {
     lcd.setCursor(X, Y);
     lcd.write(' ');
@@ -132,6 +150,7 @@ void loop() {
     }
   }
 
+  // on pressing button 2 -> go right
   else if(Btn_2_state == HIGH) {
     lcd.setCursor(X, Y);
     lcd.write(' ');
@@ -154,6 +173,7 @@ void loop() {
     }
   }
 
+  // on pressing button 3 -> go up or down
   else if(Btn_3_state == HIGH) {
     lcd.setCursor(X, Y);
     lcd.write(' ');
@@ -169,6 +189,7 @@ void loop() {
     }
   }
 
+  // on pressing button 4 -> reset snake to (0,0)
   else if(Btn_4_state == HIGH) {
     lcd.setCursor(X, Y);
     lcd.write(' ');
@@ -178,18 +199,22 @@ void loop() {
     blinkCursor(X,Y);
   }
 
+  // keep blinking snake where it already is
   else{
     lcd.setCursor(X, Y);
     lcd.write(' ');
     blinkCursor(X,Y);
   }
+
+  // update rate 100ms
   delay(100);
 }
 
+// cursor function
 void blinkCursor(int X_cor, int Y_cor){
   int X = X_cor;
   int Y = Y_cor;
-  lcd.createChar(1, block);
+  lcd.createChar(1, snake);
   lcd.setCursor(X, Y);
   lcd.write(byte(1));
 }
